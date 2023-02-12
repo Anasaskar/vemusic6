0b92ee80dcb212efa6ef5a91f75ce10ef1b9d054
Anasaskar <110418214+Anasaskar@users.noreply.github.com>
2023-02-12 05:36:14 +0200

    Update help.js

--- SlashCommands/info/help.js
+++ SlashCommands/info/help.js
@@ -38,7 +38,7 @@ module.exports = {
                 new ButtonBuilder()
                     .setStyle(ButtonStyle.Link)
                     .setLabel('Server Support')
-                    .setURL(`https://discord.gg/developer-tools`))
+                    .setURL(`https://discord.gg/m-z`))
 
         interaction.reply({ embeds: [embed], components: [row], content: `:notes: **Commands: [ ${client.commands.size} ]**`, ephemeral: true })
     },
