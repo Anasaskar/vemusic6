8b8c82f017a619e5622aa7aa03a5162bf7507596
Anasaskar <110418214+Anasaskar@users.noreply.github.com>
2023-08-28 19:54:08 +0300

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,4 +1,4 @@
-const { prefix, prefixalt } = require('../config.json');
+const { prefix, prefixalt } = process.env.prefix;
 const { Collection } = require('discord.js');
 const delay = new Collection();
 const db = require('quick.db');
